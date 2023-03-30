891c9d8fe40e8871994bab861da32670e7d14a5e
Breedwizzy <breedwizzy@yahoo..com>
2023-03-30 19:08:32 +0100

    NEW DATABASE CONFIGURATION

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-f_$y=l8^blz-p@&&s*3cy7h*e0)kxm01!2(ab8qddp+2p9c5p5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.166.251.143']
 
 
 # Application definition
@@ -181,6 +181,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
