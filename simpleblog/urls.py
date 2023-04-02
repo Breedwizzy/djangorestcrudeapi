0cf50abb97d8ee065b84a3415780d53e11d04fc1
Breedwizzy <breedwizzy@yahoo..com>
2023-04-02 18:43:40 +0100

    swagger uu

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -23,12 +23,12 @@ schema_view = get_schema_view(
 
 urlpatterns = [
 
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
-            schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger',
-                                 cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-            cache_timeout=0), name='schema-redoc'),
+    # re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+    #        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    # path('', schema_view.with_ui('swagger',
+    #                             cache_timeout=0), name='schema-swagger-ui'),
+    # re_path(r'^redoc/$', schema_view.with_ui('redoc',
+    #        cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('posts/', include('core.urls')),
     path('auth/', include('accounts.urls')),
