524f078c6a507a10b930d18807fc6bfbf675b48d
Breedwizzy <breedwizzy@yahoo..com>
2023-04-07 02:27:30 +0100

    lll

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -19,7 +19,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings -
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
